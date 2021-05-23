70c7b71679f86ba1a620a44edb2a5540d509f79a
Vinicius Perrone <perronevinicius2018@gmail.com>
2021-05-23 18:37:30 -0400

    created routes and added screens

---
+++ spotify-api/src/routes/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Routes = () => {
+    <NavigationContainer>
+        
+    </NavigationContainer>
+}
+
---
+++ spotify-api/src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Dashboard: React.FC = () => (
+    <View>
+        <Text>
+            Dashboard
+        </Text>
+    </View>
+)
+
+export default Dashboard;
