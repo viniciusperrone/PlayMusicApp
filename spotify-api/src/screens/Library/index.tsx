87120f38ffa978d2f3a490cc7ac2618ae3f46a2d
Vinicius Perrone <perronevinicius2018@gmail.com>
2021-05-25 15:02:46 -0400

    defined scope of library screen

--- spotify-api/src/screens/Library/index.tsx
+++ spotify-api/src/screens/Library/index.tsx
@@ -1,21 +1,61 @@
 import React from 'react';
 import {
-    View, Text
+    View, Text, StyleSheet
 } from 'react-native';
 
 import Background from '../../components/Background';
-import BarDown from '../../components/BarDown'
+import BarDown from '../../components/BarDown';
+
+import { Ionicons, Feather } from '@expo/vector-icons';
+
+import { Top, HeaderTop, CircleImage, MainContent } from './style';
 
 const Library: React.FC = () => {
     return (
         <Background>
-            <View>
-                <Text>Library</Text>
-            </View>
+            <Top>
+                <HeaderTop>
+                    <CircleImage />
+                    <Text style={style.title}>Sua Biblioteca</Text>
+                    <Ionicons
+                        name="search-outline"
+                        style={style.icons_search}
+                        size={28}
+                    />
+                    <Feather
+                        name="plus"
+                        style={style.icons_plus}
+                        size={28}
+                    />
+                </HeaderTop>
+            </Top>
+            <MainContent>
+                
+            </MainContent>
             <BarDown />
         </Background>
 
     )
 }
+
+const style = StyleSheet.create({
+    title: {
+        fontSize: 22,
+        color: '#fff',
+        marginLeft: 20
+    },
+    icons_search: {
+        right: 0,
+        position: 'absolute',
+        color: '#fff',
+        marginRight: 80
+    },
+    icons_plus: {
+        right: 0,
+        position: 'absolute',
+        color: '#fff',
+        marginRight: 20
+    }
+})
 
 export default Library;
---
+++ spotify-api/src/screens/Library/style.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native';
+
+export const Top = styled.View`
+    width: 100%;
+    height: 120;
+
+    background-color: #141415;
+`;
+
+export const HeaderTop = styled.View`
+    width: 100%;
+    height: 80;
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const CircleImage = styled.View`
+    width: 40;
+    height: 40;
+    background-color: #fff;
+    border-radius: 40;
+    margin-left: 10;
+`;
+
+export const MainContent = styled.View`
+    flex: 1;
+    background-color: #1E1E1F;
+`;
