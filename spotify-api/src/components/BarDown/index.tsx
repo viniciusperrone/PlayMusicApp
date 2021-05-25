14bc00a09cae911cedba5db24e47e97ba971d6f8
Vinicius Perrone <perronevinicius2018@gmail.com>
2021-05-25 13:17:06 -0400

    NavBar styled

--- spotify-api/src/components/BarDown/index.tsx
+++ spotify-api/src/components/BarDown/index.tsx
@@ -1,11 +1,70 @@
 import React from 'react';
-
-import { FooterBar } from './style';
+import { Text, StyleSheet } from 'react-native';
+import { FooterBar, ButtonNav } from './style';
+import { Entypo, Ionicons } from '@expo/vector-icons';
 
 const BarDown: React.FC = () => {
-    return(
-        <FooterBar />
+    return (
+        <FooterBar>
+            <ButtonNav>
+                <Entypo
+                    name="home"
+                    size={30}
+                    color="#fff"
+                />
+                <Text style={style.text}>
+                    Início
+                </Text>
+            </ButtonNav>
+            <ButtonNav>
+                <Ionicons
+                    name="search"
+                    size={30}
+                    color="#fff"
+                />
+                <Text style={style.text}>
+                    Buscar
+                </Text>
+            </ButtonNav>
+            <ButtonNav>
+                <Ionicons
+                    name="md-library-outline"
+                    size={30}
+                    color="#fff"
+                />
+                <Text style={style.text}>
+                    Sua Biblioteca
+                </Text>
+            </ButtonNav>
+
+            {/* <Entypo
+                name="home"
+                size={32}
+                color="#fff"
+            />
+
+            <Ionicons
+                name="search"
+                size={32}
+                color="#fff"
+            />
+
+            <Ionicons
+                name="md-library-outline"
+                size={32}
+                color="#fff"
+            /> */}
+
+        </FooterBar>
+
     )
 }
+
+const style = StyleSheet.create({
+    text: {
+        fontSize: 10,
+        color: '#FFF'
+    }
+})
 
 export default BarDown;
--- spotify-api/src/components/BarDown/style.ts
+++ spotify-api/src/components/BarDown/style.ts
@@ -5,5 +5,17 @@ export const FooterBar = styled.View`
     width: 100%;
     height: 60;
 
+    flex-direction: row;
+
     background-color: #282829; 
+`;
+
+export const ButtonNav = styled.TouchableOpacity`
+    flex: 1;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+
 `;
