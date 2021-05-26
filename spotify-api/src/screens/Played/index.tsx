a16fcc5da6175caf0ff4ed7f22cf76a4792dc773
Vinicius Perrone <perronevinicius2018@gmail.com>
2021-05-25 21:32:58 -0400

    added svg components

--- spotify-api/src/screens/Played/index.tsx
+++ spotify-api/src/screens/Played/index.tsx
@@ -1,18 +1,45 @@
 import React from 'react';
 import {
-    View, Text
+    View, Text, StyleSheet
 } from 'react-native';
 
+import { LinearGradient } from 'expo-linear-gradient';
+
 import Background from '../../components/Background';
+import ChevronIcon from '../../components/icons/Chevron';
+
+import { TopBar, TopLeft, TopMiddle, TopRight } from './style';
 
 const Played: React.FC = () => {
     return (
         <Background>
-            <View>
-                <Text>Played</Text>
-            </View>
+            <LinearGradient
+                colors={['#464769', '#1B1A1F']}
+                style={{
+                    flex: 1,
+                    paddingTop: 50,
+                }}>
+                <TopBar>
+                    <TopLeft>
+                        <ChevronIcon />
+                    </TopLeft>
+                    <TopMiddle>
+                        <Text style={style.title}>Tocando Podcast</Text>
+                    </TopMiddle>
+                    <TopRight></TopRight>
+                </TopBar>
+            </LinearGradient>
         </Background>
     )
 }
+
+const style = StyleSheet.create({
+    title: {
+        color: '#fff'
+    },
+    subtitle: {
+        color: '#fff'
+    }
+})
 
 export default Played;
---
+++ spotify-api/src/screens/Played/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const TopBar = styled.View`
+    flex-direction: row
+`;
+
+export const TopLeft = styled.View``;
+
+export const TopMiddle = styled.View``;
+
+export const TopRight = styled.View``;
+
